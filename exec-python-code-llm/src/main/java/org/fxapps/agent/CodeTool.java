19f29feab3e32b86a78241950025f5fe8a31bc20
William Siqueira <william.fatecsjc@gmail.com>
2025-03-08 01:15:05 -0300

    Simplifying the code

--- exec-python-code-llm/src/main/java/org/fxapps/agent/CodeTool.java
+++ exec-python-code-llm/src/main/java/org/fxapps/agent/CodeTool.java
@@ -1,9 +1,7 @@
 package org.fxapps.agent;
 
 import java.io.StringWriter;
-
 import org.python.util.PythonInterpreter;
-
 import dev.langchain4j.agent.tool.Tool;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -11,11 +9,13 @@ import jakarta.enterprise.context.ApplicationScoped;
 public class CodeTool {
     @Tool("Execute the given Python code")
     public String execCode(String code) {
-        try (PythonInterpreter pyInterp = new PythonInterpreter()) {
-            StringWriter output = new StringWriter();
-            System.out.println("Running the following code: \n" + "*".repeat(10) + "\n" + code + "\n"+  "*".repeat(10) );
+        try (var pyInterp = new PythonInterpreter()) {
+            var output = new StringWriter();
+
+            System.out.println("Running the following code: \n" + "*".repeat(10) + "\n" + code + "\n" + "*".repeat(10));
             pyInterp.setOut(output);
             pyInterp.exec(code);
+
             var result = output.toString().trim();
             System.out.println("Result is: " + result);
             return result;
--- exec-python-code-llm/src/main/java/org/fxapps/agent/TestService.java
+++ exec-python-code-llm/src/main/java/org/fxapps/agent/TestService.java
@@ -1,14 +1,12 @@
 package org.fxapps.agent;
 
 import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
 @RegisterAiService(tools = { CodeTool.class })
 public interface TestService {
 
     @SystemMessage("You are an assistant that generates Python code and execute it.")
-    @UserMessage("")
     String input(String input);
 
 }
